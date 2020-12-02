56bbc7f3304d89b59c3fecd118743cf0fadaa471
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2020-12-02 11:22:48 +0100

    #1 cleaned template and created project and task components

--- projekt/src/app/app.module.ts
+++ projekt/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProjectComponent, TaskComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
