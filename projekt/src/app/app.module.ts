17df49aabd0de7dd2eb40d7c549c275bc79de993
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2020-12-07 15:31:11 +0100

    #5 added angular materias

--- projekt/src/app/app.module.ts
+++ projekt/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { ProjectComponent } from './project/project.component';
 import { TaskComponent } from './task/task.component';
 import { ProgressbarComponent } from './progressbar/progressbar.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, ProjectComponent, TaskComponent, ProgressbarComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
