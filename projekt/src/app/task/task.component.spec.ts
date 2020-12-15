b2fc564dcb06c6ace7c0f6ff4ae8f767710c65ed
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2020-12-15 13:16:00 +0100

    #12 implemented unit tests for task.component

--- projekt/src/app/task/task.component.spec.ts
+++ projekt/src/app/task/task.component.spec.ts
@@ -1,24 +1,42 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppModule } from '../app.module';
+import { Project } from '../models/project';
+import { Task } from '../models/task';
 
 import { TaskComponent } from './task.component';
 
 describe('TaskComponent', () => {
   let component: TaskComponent;
-  let fixture: ComponentFixture<TaskComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TaskComponent],
-    }).compileComponents();
-  });
+  // testdata for the tests
+  const testProject = new Project('test Project');
+  testProject.tasks = [new Task('test task 1'), new Task('test task 2')];
+  testProject.tasks[0].status = 1;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TaskComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      imports: [AppModule],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }, TaskComponent],
+    });
+    component = TestBed.inject(TaskComponent);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('test getTasks: should provide task data from backend.service', () => {
+    expect(component.getTasks(testProject)).toEqual(testProject.tasks);
+  });
+
+  it('test addTask: should send task name to backend.service', () => {
+    component.addTask({ description: 'test task', status: 0 }, testProject);
+    expect(testProject.tasks.length).toEqual(3);
+  });
+
+  it('test changeStatus: should send information to change status to backend.service', () => {
+    component.changeStatus(1, testProject.tasks[1]);
+    expect(testProject.tasks[1].status).toEqual(1);
   });
 });
