65de42ddb764d055b386b30857848352c65c66a2
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2020-12-16 16:08:39 +0100

    #14 added more unit test to project.component

--- projekt/src/app/project/project.component.spec.ts
+++ projekt/src/app/project/project.component.spec.ts
@@ -15,12 +15,34 @@ describe('ProjectComponent', () => {
     component = TestBed.inject(ProjectComponent);
   });
 
-  it('test getProject: should provide project data from backend ', () => {
+  it('test viewProjects: should provide project data from backend ', () => {
     expect(component.viewProjects()).toEqual(component.backendService.projects);
   });
 
+  it('test viewProjects: should call getProjects form backend.service ', () => {
+    const spy = jest.spyOn(component.backendService, 'getProjects');
+    expect(spy).toHaveBeenCalled;
+  });
+
+  it('test viewProjects: should not be empty ', () => {
+    expect(component.viewProjects().length).toEqual(component.backendService.projects.length);
+  });
+
   it('test addProject: should send new project data to backend.service', () => {
+    const length = component.backendService.projects.length;
     component.addProject('test name');
-    expect(component.backendService.projects.length).toEqual(3);
+    expect(component.backendService.projects.length).toEqual(length + 1);
+  });
+
+  it('test addProject: should call createProject from backend.service', () => {
+    const spy = jest.spyOn(component.backendService, 'createProject');
+    component.addProject('test spy');
+    expect(spy).toHaveBeenCalled;
+  });
+
+  it('test addProject: should not create a new project if name is empty', () => {
+    const length = component.backendService.projects.length;
+    component.addProject('');
+    expect(component.backendService.projects.length).toEqual(length);
   });
 });
